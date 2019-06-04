8e0c6dee7951641f8e5b447e60a389310303400b
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-06-03 17:34:31 -1000

    16

--- exercises.js
+++ exercises.js
@@ -400,8 +400,12 @@ for(i = 0; i < 5; i++){
  * #16
  * Create a for loop that will iterate and console.log each item in the array below:
 */
-  var myFavFoods = ["lemon bar", "carrot cake", "nachos", "bacon cheeseburger", "ramen", "sweet potato fries", "chimichanga"];
 
+var myFavFoods = ["lemon bar", "carrot cake", "nachos", "bacon cheeseburger", "ramen", "sweet potato fries", "chimichanga"];
+
+for(i = 0; i < myFavFoods.length; i++){
+  console.log(myFavFoods[i]);
+}
 
 /*
  * #17
